60624f15d0221271ec2b794245debfe4af400ab7
Akshay Joshi <joshi.a@gmail.com>
2012-03-03 15:10:35 -0500

    Revert "Added getter/setter for round duration."
    This reverts commit dc06e156b272017cdbe41de38833a1f4b5b5ef96.

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,11 +1,2 @@
 class Round < ActiveRecord::Base
-  validates_presence_of :starts_at
-
-  def duration
-    read_attribute(:ends_at) - read_attribute(:starts_at)
-  end
-
-  def duration=(time)
-    write_attribute(:ends_at, read_attribute(:starts_at) + time)
-  end
 end
