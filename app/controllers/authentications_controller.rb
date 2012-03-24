19bc1be2632fcfc9ce94f415dd7fedbbabe4810e
Akshay Joshi <me@akshayjoshi.com>
2012-03-24 17:58:07 -0400

    Add authentications controller.

---
+++ app/controllers/authentications_controller.rb
@@ -0,0 +1,9 @@
+class AuthenticationsController < ApplicationController
+  def index
+    @authentications = Authentication.all
+  end
+
+  def create
+    render :text => request.env["omniauth.auth"].to_yaml
+  end
+end
