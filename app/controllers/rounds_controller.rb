7721bed8d680194e0bf6987f45878b280b98bb7d
Akshay Joshi <me@akshayjoshi.com>
2012-03-24 03:14:36 -0400

    Add some default controller actions for rounds.

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,2 +1,23 @@
 class RoundsController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Round.all
+  end
+
+  def show
+    respond_with Round.find(params[:id])
+  end
+
+  def create
+    respond_with Round.create(params[:round])
+  end
+
+  def update
+    respond_with Round.update(params[:id], params[:round])
+  end
+
+  def destroy
+    respond_with Round.destroy(params[:id])
+  end
 end
