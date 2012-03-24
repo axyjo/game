5f1d929419f2df36346f24754f11c1d9a548bc02
Akshay Joshi <me@akshayjoshi.com>
2012-03-24 17:28:14 -0400

    Add omniauth initializer.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+require 'omniauth-google-oauth2'
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
+  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], {access_type: 'online', approval_prompt: ''}
+end
