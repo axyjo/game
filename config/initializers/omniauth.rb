be4cc1c658ee6ee4e38c29c2e105cecffef2b6e2
Akshay Joshi <me@akshayjoshi.com>
2012-03-24 19:50:25 -0400

    Add omniauth-identity as a provider.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 require 'omniauth-google-oauth2'
+require 'omniauth-identity'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], {access_type: 'online', approval_prompt: ''}
+  provider :identity, :fields => [:email]
 end
