aa8d82bbe7136eb13ab982eb8a44ca33d77f7e87
Akshay Joshi <me@akshayjoshi.com>
2012-03-25 01:13:48 -0400

    Add a database cleaner to ensure a consistent DB state when running tests.

--- spec/minitest_helper.rb
+++ spec/minitest_helper.rb
@@ -1,3 +1,11 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "minitest/autorun"
+
+# Database cleaner.
+DatabaseCleaner.strategy = :truncation
+class MiniTest::Spec
+  before :each do
+    DatabaseCleaner.clean
+  end
+end
