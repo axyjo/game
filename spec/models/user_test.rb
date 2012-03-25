c8d1dc7ec9a717f3d91271ac8b769fad41811a69
Akshay Joshi <me@akshayjoshi.com>
2012-03-25 01:13:20 -0400

    Update some user tests.

--- spec/models/user_test.rb
+++ spec/models/user_test.rb
@@ -9,11 +9,11 @@ describe User do
   it "validates when the email address is valid" do
     user = User.create(email: "bob@example.com")
     user.valid?.must_equal true
+  end
 
+  it "does not validate when the email does not contain a TLD" do
     user = User.create(email: "alice@localhost")
-    user.valid?.must_equal true
-
-
+    user.valid?.must_equal false
   end
 
   it "validates when there is a plus in the email address" do
@@ -21,9 +21,8 @@ describe User do
     user.valid?.must_equal true
   end
 
-  it "validates when the email host is an IP address" do
+  it "does not validate when the email host is an IP address" do
     user = User.create(email: "alice@127.0.0.1")
-    user.valid?.must_equal true
+    user.valid?.must_equal false
   end
-
 end
