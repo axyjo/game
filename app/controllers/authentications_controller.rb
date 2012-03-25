a93fee536b8bd699c339576a64cbe617a433fa6e
Akshay Joshi <me@akshayjoshi.com>
2012-03-24 20:43:03 -0400

    Enhance authentications controller logic for logging in and creating an account.

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,9 +1,27 @@
 class AuthenticationsController < ApplicationController
+  respond_to :json
   def index
-    @authentications = Authentication.all
+    if current_user
+      respond_with current_user.authentications.all
+    else
+      respond_with []
+    end
   end
 
   def create
-    render :text => request.env["omniauth.auth"].to_yaml
+    omniauth = request.env["omniauth.auth"]
+    auth = Authentication.find_by_provider_and_uid(omniauth['provider'], auth['uid'])
+    if auth
+      session[:user_id] = auth.user_id
+      redirect_to "/"
+    elsif current_user
+      current_user.authentications.create(:provider => auth['provider'], :uid => auth['uid'])
+    else
+      u = User.create(:email => auth['info']['email'])
+      if u.save
+        session[:user_id] = u.id
+        redirect_to "/"
+      end
+    end
   end
 end
