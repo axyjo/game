da96d27d922915f2ed0acaa9b11835f997c99dd2
Akshay Joshi <me@akshayjoshi.com>
2012-03-25 01:16:38 -0400

    Fix a round test.

--- spec/models/round_test.rb
+++ spec/models/round_test.rb
@@ -1,8 +1,13 @@
 require "minitest_helper"
 
 describe Round do
-  it "does not validate when both duration and ends_at are specified" do
-    round = Round.create(starts_at: Time.now, ends_at: 4.minutes.from_now, duration: 300.seconds)
-    round.valid?.must_equal false
+  it "uses duration when both end time and duration are specified" do
+    start_time = Time.now
+    end_time = start_time + 4.minutes
+    duration_time = start_time + 5.minutes
+
+    round = Round.create(starts_at: start_time, ends_at: end_time, duration: 5.minutes)
+    round.valid?.must_equal true
+    round.ends_at.must_equal duration_time
   end
 end
