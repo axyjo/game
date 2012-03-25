54fdd2f7df88279eb59a0085d9fd4e3994613e95
Akshay Joshi <me@akshayjoshi.com>
2012-03-25 00:44:23 -0400

    Fix test.

--- spec/models/round_test.rb
+++ spec/models/round_test.rb
@@ -2,7 +2,7 @@ require "minitest_helper"
 
 describe Round do
   it "does not validate when both duration and ends_at are specified" do
-    round = Round.create!(ends_at: 4.minutes.from_now, duration: 300.seconds)
+    round = Round.create(starts_at: Time.now, ends_at: 4.minutes.from_now, duration: 300.seconds)
     round.valid?.must_equal false
   end
 end
